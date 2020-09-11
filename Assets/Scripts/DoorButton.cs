286fbdb5aaf76c38cd08bd81ae08bf11de15d339
propredator23 <christiandash@hotmail.com>
2020-09-11 13:22:56 -0700

    ForGitHubDemo
    ForGitHubDemo

---
+++ Assets/Scripts/DoorButton.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    public GameObject door;                     //This is a reference to the door we want to open that we will be able to set on the editor
+}
---
+++ Assets/Scripts/SpawnArrow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArrow : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public GameObject [] locations;
+    public int location;
+
+    void Spawn()
+    {
+        Instantiate(arrowPrefab, transform.position + new Vector3(0,3,1), Quaternion.identity);
+    }
+}
