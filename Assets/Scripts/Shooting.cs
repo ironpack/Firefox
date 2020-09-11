4fb63b1dd000be76d33df1f4d02e62a71e7ba665
propredator23 <christiandash@hotmail.com>
2020-09-11 13:56:19 -0700

    Shooting Mechanic
    Added Shooting Mechanic

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    GameObject bullet;
+    public Transform spawnLocation;
+    public float fireRate;
+    private float Timer;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Timer < 0)
+        {
+            bullet = Instantiate(bulletPrefab, spawnLocation.position, spawnLocation.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = spawnLocation.up * 10;
+            Timer=fireRate;
+        }
+        Timer -= Time.deltaTime;
+    }
+}
